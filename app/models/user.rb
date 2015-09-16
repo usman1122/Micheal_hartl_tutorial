1dee3ef5749d379d01d94452d6bc0eddf74bc1da
vagrant <vagrant@bootcamp>
2015-09-16 09:35:04 +0000

    chapter 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ActiveRecord::Base
+	
 	 validates :name,  presence: true, length: { maximum: 42 }
+
+# email
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 42 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+                   before_save {self.email=email.downcase}  #email call back before save
+        
+
+# password
+     has_secure_password
+     validates :password, presence: true, length: {minimum: 6}
 
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get Home" do
+  test "should get home" do
     get :home
     assert_response :success
     assert_select "title","Home | Ruby on Rails Tutorial Sample App"
